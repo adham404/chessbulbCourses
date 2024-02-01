04780c8a9abae8c4415f913af5e2458c48bf64dd
Adham <adham_mohsen@outlook.com>
2024-02-01 09:34:04 +0200

    minor modifications

--- client/src/utils/info.ts
+++ client/src/utils/info.ts
@@ -1,4 +1,4 @@
 export default {
-    // BackendUrl: "https://hammerhead-app-sv7hf.ondigitalocean.app/api/",
-    BackendUrl: "http://localhost:8080/api/",
+    BackendUrl: "https://squid-app-zkrr2.ondigitalocean.app/api/",
+    // BackendUrl: "http://localhost:8080/api/",
   };
